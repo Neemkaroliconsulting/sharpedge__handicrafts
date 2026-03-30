a8c1883f6230ab579e4189dfd8aff782d2935a7f
Neemkaroliconsulting <paritoshrohatgi@gmail.com>
2026-03-30 15:17:56 +0530

    Update description_wizard.py

--- product_packaging_dimensions/wizard/description_wizard.py
+++ product_packaging_dimensions/wizard/description_wizard.py
@@ -21,7 +21,7 @@ class DescriptionSelectWizard(models.TransientModel):
     line_ids = fields.Many2many(
     "account.move.line",
     string="Select Products",
-    domain="[('move_id', '=', context.get('active_id')), ('display_type', '=', False), ('tax_line_id', '=', False)]"
+   
 )
     output_format = fields.Selection(
         [
